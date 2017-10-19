a0db81b65f1cb6289a603871d13f793e950a5f4c
Mark Zeidan <mfzeidan@gmail.com>
2017-10-19 16:11:58 -0400

    Update Zeiden_HW2.r

--- Zeiden_HW2.r
+++ Zeiden_HW2.r
@@ -1,149 +1,67 @@
 library(AppliedPredictiveModeling)
 library(caret)
 data(permeability)
+library(elasticnet)
 library("pls")
-#
-# (B)
-#
-
-## here we are filtering out columns with low frequencies 
-ncol(fingerprints)
-nzv_cols <- nearZeroVar(fingerprints)
-if(length(nzv_cols) > 0) fingerprints <- fingerprints[, -nzv_cols]
-ncol(fingerprints) ## 388 predictors remain from the original 1107 predictors (719 removed)
-
-#
-# (C)
-#
 
 
-## split the data into a test and training data set
-## preprocess the data and tune a PLS model
+## B
+set.seed(123)
 
-## 75% of the sample size
-## this is to split the training and test sets up from the data set
+data(permeability)
+zeroColumns = nearZeroVar(fingerprints)
+print( sprintf("Found %d zero variance columns from %d",length(zeroColumns), dim(fingerprints)[2] ) )
+fingerprints = fingerprints[,-zeroColumns] # drop these zero variance columns 
+
+## 388 predictors remain from the original 1107 predictors (719 removed)
+
+## C
+
 smp_size <- floor(0.75 * nrow(fingerprints))
 
 ## set the seed to make your partition reproductible
 set.seed(123)
 
-## breaking up the data into training and test data sets for the fingerprint and
-## permeability data set
-
-# getting samples
 
 train_ind <- sample(seq_len(nrow(fingerprints)), size = smp_size)
 train_ind_perm <- sample(seq_len(nrow(permeability)), size = smp_size)
-
-## test sets
-
-## permeability test and training data sets
-train_perm <- permeability[train_ind_perm,]
-test_perm <- permeability[-train_ind_perm,]
-## fingerprint test and training data set
 train_fp <- fingerprints[train_ind, ]
 test_fp <- fingerprints[-train_ind, ]
-
-
-## PLS model
-
-## 10 folds cross validation
-
-ctrl <- trainControl(method = "cv", number = 10)
-
-## need to turn this into a df for the plsFit 
-df_test_fp <- data.frame(test_fp)
-df_train_fp <- data.frame(train_fp)
-plsFit = plsr(train_perm ~ . , data = df_train_fp, ncomp = 20)
-
-## try on test data (316)
-plsPred = predict(plsFit, df_test_fp, ncomp = 20)
-
-plsValue = data.frame(obs = test_perm, pred = plsPred[,,1])
-
-defaultSummary(plsValue) 
-## RMSE     Rsquared          MAE 
-#3 2.070195e+01 4.289488e-04 1.608290e+01 
-
-plsModel <- train(x=train_fp, y = train_perm,
-                  method="pls",
-                  tuneLength=20,
-                  trControl = ctrl,
-                  preProc= c("center", "scale"))
-plsModel
-
-## Again, try 10 folds cross validation
-set.seed(123)
-pls <- train(x = df_train_fp, y = train_perm, method = "pls", tuneGrid = expand.grid(ncomp = 20), trControl = ctrl)
-pls
-
-testpls <- data.frame(obs = test_perm, pred = predict(pls, df_test_fp))
-
-defaultSummary(testpls)
-## RMSE     Rsquared          MAE 
-## 2.070195e+01 4.289488e-04 1.608290e+01 
-
-
-## Model tuning
-## Run PLS and PCR on solubility data and compare results with 10 folds CV
-
-## tuning model on PLS and PCR
+train_perm <- permeability[train_ind,]
+test_perm <- permeability[-train_ind,]
 
 set.seed(123)
-plsTune <- train(x = df_train_fp, y = train_perm, method = "pls", tuneGrid = expand.grid(ncomp = 1:20), trControl = ctrl)
-plsTune
 
-testResultsPLS <- data.frame(obs = test_perm, pred = predict(plsTune, df_test_fp))
+control <- trainControl(method="cv")
 
-### PLS test results
+plsModel_training = train( train_fp, train_perm, method="pls",
+                   tuneLength=40, 
+                   preProcess=c("center","scale"), 
+                   trControl=control )
 
-defaultSummary(testResultsPLS)
+plsModel_training
 
-## RMSE         Rsquared   MAE 
-## 18.16830210  0.07153656 15.03183106 
-
-
-set.seed(123)
-pcrTune <- train(x = df_test_fp, y = test_perm, method = "pcr", tuneGrid = expand.grid(ncomp = 1:35), trControl = ctrl)
-pcrTune      
-
-testResultsPCR <- data.frame(obs = test_perm, pred = predict(pcrTune, df_test_fp))
-defaultSummary(testResultsPCR)
-
-## RMSE      Rsquared  MAE 
-## 5.5789024 0.8802798 4.0122384 
+y_hat_train = predict(plsModel_training, newdata = train_fp)
+r2_pls_train = cor(y_hat_train, train_perm, method="pearson")^2
+rmse_pls_train=sqrt( mean( y_hat_train-train_perm)^2)
+plsPR_train <- postResample(y_hat_train, obs=train_perm)
+plsPR_train
 
 
+plot(x=plsModel_training$results$ncomp, y= plsModel_training$results$Rsquared)
 
-plsResamples <- plsTune$results
-plsResamples$Model <- "PLS"
-pcrResamples <- pcrTune$results
-pcrResamples$Model <- "PCR"
-plsPlotData <- rbind(plsResamples, pcrResamples)
+##How many latent variables are optimal - 7
+# what is the corresponding resampled estimate of R2 - 0.79
 
-plsPlotData
+##  (D)
 
-xyplot(RMSE ~ ncomp,
-       data = plsPlotData, ## note this is both pls and pcr data
-       #aspect = 1,
-       xlab = "# Components",
-       ylab = "RMSE (Cross-Validation)",       
-       col = c("blue","red"),
-       auto.key = list(columns = 2),
-       groups = Model,
-       type = c("o", "g"))
+y_hat = predict(plsModel, newdata = test_fp)
+r2_pls = cor(y_hat, test_perm, method="pearson")^2
+rmse_pls=sqrt( mean( y_hat-test_perm)^2)
+plsPR <- postResample(y_hat, obs=test_perm)
+plsPR
 
-
-# variable importance - PLS  Fig. 6.14
-plsImp <- varImp(plsTune, scale = FALSE)
-plot(plsImp, top = 25, scales = list(y = list(cex = .95)))
-
-# variable importance - PCR
-pcrImp <- varImp(pcrTune, scale = FALSE)
-plot(pcrImp, top = 25, scales = list(y = list(cex = .95)))
-
-
-## do a MARS analysis
+# Test set estimate of R2 is 0.465
 
 
 ###################
@@ -151,56 +69,76 @@ plot(pcrImp, top = 25, scales = list(y = list(cex = .95)))
 
 # exercise 7.4
 
-# install.packages("AppliedPredictiveModeling")
+#II. (2.5 point) Chapter 7, Exercise 7.4 (p.170). For this problem, 
+#please try at least 3 out of the 4 methods we have discussed: neurnal networks, SVM, MARS, and kNN.
+
+
+library(caret)
 library(AppliedPredictiveModeling)
-library(e1071) # misc library including skewness function
-library(corrplot)
-library(lattice)
-library(caret) 
 
-## training set and test set have been assigned --> solTrainX
-## Also transformation has been done  --> solTrainXtrans
-data(solubility)
-library(mlbench)
-library(nnet)
-#install.packages("earth")
-library(earth)
-#install.packages("kernlab")
-library(kernlab)
-#install.packages("pROC")
-library(pROC)
+set.seed(123)
+## KNN Model
 
+knnModel = train(x = train_fp, 
+                 y = train_perm, 
+                 method = "knn",
+                 preProc = c("center","scale"),
+                 tuneLength = 10)
+knnPred = predict(knnModel, newdata = train_fp)
+knnPR = postResample(pred=knnPred, obs = train_perm)
+rmses_training = c(knnPR[1])
+r2s_training = c(knnPR[2])
+methods = c("KNN")
 
-set.seed(100)
-
-# generate training data
-trainingData = mlbench.friedman1(200,sd=1)
-
-## trainingData$x contains a 200x10 sample predictor data
-## Note that 5 of them are related to the response and the remaining five are unrelated random variables 
-dim(trainingData$x)
-## trainingData$y contains 200 response/outcome data
-trainingData$y
+knnPred = predict(knnModel, newdata=test_fp)
+knnPR = postResample(pred = knnPred, obs = test_perm)
+knnPR
+#RMSE  Rsquared       MAE 
+#14.117169  0.403577  9.516708 
 
 
-## We convert the 'x' data from a matrix to a data frame
-## One reason we do this is that this will give the columns names
-trainingData$x = data.frame(trainingData$x)
-## Look at the data using
-featurePlot(trainingData$x, trainingData$y)
+### neural network
 
-## Now generate a large test data set to estimate the error rate
-testData = mlbench.friedman1(5000,sd=1)
-testData$x = data.frame(testData$x)     ## 5,000 x 10 data samples
+  nnGrid = expand.grid( .decay = c(0,0.01,0.1), .size=1:10, .bag=FALSE)
+  set.seed(123)
+  nnetModel = train(x = train_fp, y = train_perm, method = "nnet", preProc = c("center","scale"),
+                    linout = TRUE, trace=FALSE, 
+                    MaxNWts = 10*(ncol(train_fp)+1) + 10 + 1, maxit = 500)
+  
+  nnetPred = predict(nnetModel, newdata = train_fp)
+  nnetPR = postResample(pred = nnetPred, obs = train_perm)
+  nnetPR ## get the r2 from this
+  
+  #RMSE   Rsquared        MAE 
+  #10.1196546  0.5232648  6.8136310 
+
+  nnetPred_test = predict(nnetModel, newdata = test_fp)
+  nnetPR_test = postResample(pred = nnetPred_test, obs = test_perm)
+  nnetPR_test # get the r2 from this
+  
+ # RMSE   Rsquared        MAE 
+ #16.3563674  0.2074993 11.3851790 
 
 
 
-set.seed(100)
-# Without specifying train control, the default is bootstrap 
-knnModel = train(x=trainingData$x, y=trainingData$y, method="knn",
-                 preProc=c("center","scale"),
-                 tuneLength=10)
-knnModel
+## support vector machine
 
-# plot the RMSE performance against the k
-plot(knnModel$results$k, knnModel$results$RMSE, type="o",xlab="# neighbors",ylab="RMSE", main="KNNs for Friedman Benchmark")
+set.seed(123)
+svmModel = train(x=train_fp, y = train_perm, 
+                 method = "svmRadial", 
+                 preProc = c("center","scale"),
+                 tuneLength = 20)
+svmPred_train = predict(svmModel, newdata = train_fp)
+svmPR_train = postResample(pred=svmPred_train, obs = train_perm)
+svmPR_train
+
+#RMSE  Rsquared       MAE 
+#1.5029363 0.9963344 1.4564040 
+
+svmPred = predict(svmModel, newdata=test_fp)
+svmPR = postResample(pred = svmPred, obs = test_perm)
+svmPR
+
+#RMSE    Rsquared         MAE 
+#17.90744398  0.06586038 13.95337609 
+
